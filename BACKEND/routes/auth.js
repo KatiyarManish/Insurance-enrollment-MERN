b7af2a8d7487e1f50fdd1054d150a55ff5753eea
trendmanishGIT <trendymanish@gmail.com>
2023-02-21 18:19:07 +0530

    verification email done

--- BACKEND/routes/auth.js
+++ BACKEND/routes/auth.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { userValidator, validate } = require("../middleware/validator");
 
-const { getUsers, createUser } = require("../controllers/auth");
+const { getUsers, createUser, verifyEmail } = require("../controllers/auth");
 
 router.get("/", getUsers);
 router.post("/register", userValidator, validate, createUser);
+router.post("/verify-email", verifyEmail);
 
 module.exports = router;
