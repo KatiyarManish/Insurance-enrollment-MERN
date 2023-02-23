7d7cc7ca938e1d5dfde8ee8578a8a49314296c39
trendmanishGIT <trendymanish@gmail.com>
2023-02-23 22:15:27 +0530

    express-async-errors added

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+require("express-async-errors");
+const errorHandler = require("./middleware/error");
 
 require("./DB/dbConnect");
 const app = express();
@@ -9,6 +11,7 @@ const userRouter = require("./routes/auth");
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/api/v1/user", userRouter);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`app listening on this ${port}`);
